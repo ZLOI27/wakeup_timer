8ea21a82faafb274a9d59508d127db7fcbebf7ef
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-05-03 23:22:00 +0300

    add: the ability to enter time

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import os
 
 
 def main() -> None:
-    os.system('sudo rtcwake -m mem -s 20')
-    pass
+    """
+    This programm set timer to wakeup system from suspend.
+    """
+    time = input('Enter time hh:mm: ').strip()
+    os.system(f'sudo rtcwake -u --date {time}')
+    os.system('sudo systemctl suspend') # Temporary line
+    os.system('xdg-open https://rbc.ru')
 
 
 if __name__ == '__main__':
