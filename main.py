e97ff6400def35c4e17c9435924e124d984293c6
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-05-04 09:41:41 +0300

    add: library selenium and use it

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-import socket
-import os
-import time
+import socket, os, time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 def main() -> None:
     """
     This programm set timer to wakeup system from suspend.
     """
-    time_wakeup = input("Enter time hh:mm: ").strip()
-    os.system(f"sudo rtcwake -u --date {time_wakeup}")
-    os.system("sudo systemctl suspend") # Temporary line
+    time_wakeup = input("Enter time hh:mm: ").strip() # It is required to add check correct input
+    os.system(f"sudo rtcwake -u --date {time_wakeup}") # It is required to add 'try except'
+    os.system("sudo systemctl suspend") # Temporary line -----
     time.sleep(5)
     while True:
         if check_internet():
-            os.system("xdg-open https://www.rbc.ru")
+            driver = webdriver.Firefox()
+            driver.get("https://www.rbc.ru")
             break
         else:
             print("Disable internet connection")
-            time.sleep(3)
+            time.sleep(2)
 
 
-def check_internet(host='8.8.8.8', port=53, timeout=3):
+def check_internet(host='8.8.8.8', port=53, timeout=3) -> bool:
     """
     Testing internet conection with DNS-server Google (8.8.8.8)
     """
