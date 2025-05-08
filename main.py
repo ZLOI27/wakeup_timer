64103ce25fdf63eb59748ec45b358c5c82b29ee9
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-05-08 10:04:55 +0300

    add: fullscreen and volume control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import socket, os, time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
 
 
 def main() -> None:
@@ -13,9 +14,22 @@ def main() -> None:
     time.sleep(5)
     while True:
         if check_internet():
-            os.system("pactl set-sink-volume @DEFAULT_SINK@ 50%")
+            os.system("pactl set-sink-volume @DEFAULT_SINK@ 20%")
             driver = webdriver.Firefox()
-            driver.get("https://www.rbc.ru")
+            driver.get('https://www.rbc.ru')
+            driver.maximize_window()
+            time.sleep(2)
+            full_screen_btn = driver.find_element(By.CLASS_NAME, 'video-player__controls__fullscreen')
+            full_screen_btn.click()
+            full_screen_btn = driver.find_element(By.CLASS_NAME, 'video-player__controls__fullscreen')
+            full_screen_btn.click()
+            time.sleep(25)
+            volume_btn = driver.find_element(By.CLASS_NAME, 'video-player__controls__volume')
+            volume_btn.click()
+            os.system("pactl set-sink-volume @DEFAULT_SINK@ 50%")
+            time.sleep(20) #3600
+            driver.quit()
+            os.system("systemctl suspend")
             break
         else:
             print("Disable internet connection")
@@ -23,9 +37,7 @@ def main() -> None:
 
 
 def check_internet(host='8.8.8.8', port=53, timeout=3) -> bool:
-    """
-    Testing internet conection with DNS-server Google (8.8.8.8)
-    """
+    """Testing internet conection with DNS-server Google (8.8.8.8)."""
     try:
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
