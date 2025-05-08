a7e183ad8fb8c19176d89061943a800615b5bfca
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-05-08 20:47:58 +0300

    refactor: add two functions and costants

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-import socket, os, time
+import socket
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 
+WEBSITE = "https://www.rbc.ru"
+TIME_OF_NEWS = 20
+
+
 def main() -> None:
     """
-    This programm set timer to wakeup system from suspend and open browser.
+    This programm set timer to wakeup system from suspend and opens
+    a browser and website with news.
     """
-    time_wakeup = input("Enter time hh:mm: ").strip() # It is required to add check correct input
-    os.system(f"sudo rtcwake -u --date {time_wakeup}") # It is required to add 'try except'
-    os.system("sudo systemctl suspend") # Temporary line -----
+    time_wakeup = input(
+        "Enter time hh:mm: "
+    ).strip()  # It is required to add check correct input
+    os.system(
+        f"sudo rtcwake -u --date {time_wakeup}"
+    )  # It is required to add 'try except'
+    os.system("sudo systemctl suspend")  # Temporary line -----
     time.sleep(5)
     while True:
         if check_internet():
             os.system("pactl set-sink-volume @DEFAULT_SINK@ 20%")
             driver = webdriver.Firefox()
-            driver.get('https://www.rbc.ru')
+            driver.get(WEBSITE)
             driver.maximize_window()
             time.sleep(2)
-            full_screen_btn = driver.find_element(By.CLASS_NAME, 'video-player__controls__fullscreen')
-            full_screen_btn.click()
-            full_screen_btn = driver.find_element(By.CLASS_NAME, 'video-player__controls__fullscreen')
-            full_screen_btn.click()
+            full_screen_on(driver)
             time.sleep(25)
-            volume_btn = driver.find_element(By.CLASS_NAME, 'video-player__controls__volume')
-            volume_btn.click()
+            site_sound_on(driver)
             os.system("pactl set-sink-volume @DEFAULT_SINK@ 50%")
-            time.sleep(20) #3600
+            time.sleep(600)
+            os.system("pactl set-sink-volume @DEFAULT_SINK@ 75%")
+            time.sleep(TIME_OF_NEWS)
             driver.quit()
             os.system("systemctl suspend")
             break
@@ -36,7 +45,7 @@ def main() -> None:
             time.sleep(2)
 
 
-def check_internet(host='8.8.8.8', port=53, timeout=3) -> bool:
+def check_internet(host="8.8.8.8", port=53, timeout=3) -> bool:
     """Testing internet conection with DNS-server Google (8.8.8.8)."""
     try:
         socket.setdefaulttimeout(timeout)
@@ -46,5 +55,23 @@ def check_internet(host='8.8.8.8', port=53, timeout=3) -> bool:
         return False
 
 
-if __name__ == '__main__':
+def full_screen_on(driver: object) -> None:
+    full_screen_btn = driver.find_element(
+        By.CLASS_NAME, "video-player__controls__fullscreen"
+    )
+    full_screen_btn.click()
+    full_screen_btn = driver.find_element(
+        By.CLASS_NAME, "video-player__controls__fullscreen"
+    )
+    full_screen_btn.click()
+
+
+def site_sound_on(driver: object) -> None:
+    volume_btn = driver.find_element(
+        By.CLASS_NAME, "video-player__controls__volume"
+    )
+    volume_btn.click()
+
+
+if __name__ == "__main__":
     main()
